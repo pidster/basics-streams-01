b4b18423695b8aa36d17ca2a706d7c7861df50f6
Stuart Williams <pidster@users.noreply.github.com>
2019-08-28 10:36:50 +0100

    add example solution

--- src/main/java/org/example/streams/Application.java
+++ src/main/java/org/example/streams/Application.java
@@ -1,5 +1,7 @@
 package org.example.streams;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -18,6 +20,28 @@ public class Application {
          * and Lambas.
          *
          */
+
+        List.of(args)
+            .stream()
+            .map(Application::convert)
+            .forEach(System.out::println);
     }
+
+    private static Argument convert(String argument) {
+
+        String[] pair = argument.split("=");
+
+        String name = pair[0].replaceAll("^-+", "");
+        Object value = Boolean.TRUE;
+        Class<?> type = Boolean.class;
+
+        if (pair.length == 2) {
+            value = pair[1];
+            type = String.class;
+        }
+
+        return new Argument(name, value, type);
+    }
+
 
 }
